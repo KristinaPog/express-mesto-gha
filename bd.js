b40c185ff78f6bfb8eb53fff228fae71981a77c4
Kristina Pogodina <pogodina.kristi@yandex.ru>
2023-07-17 00:42:45 +0300

    Исправления после ревью

--- bd.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  users: [
-    { name: 'Мария', about: "22", avatar: "32"},
-    { name: 'Виктор', about: "22", avatar: "32" },
-    { name: 'Анастасия', about: "22", avatar: "32" },
-    { name: 'Алексей', about: "22", avatar: "32" }
-  ]
-};
