5acc6e5db94ab4bef724691a2a8c51ad221978aa
Kristina Pogodina <pogodina.kristi@yandex.ru>
2023-08-01 12:45:23 +0300

    Исправлено удаление карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -32,7 +32,7 @@ module.exports.deleteCard = (req, res, next) => {
     .then((card) => {
       const owner = card.owner.toString();
       if (req.user._id === owner) {
-        card.remove().then(res.status(STATUS_CODE_OK).send(card));
+        Card.findByIdAndRemove(req.params.cardId).then(res.status(STATUS_CODE_OK).send(card));
       } else { next(new Forbitten({ message: 'Эта карточка принадлежит другому пользователю' })); }
     })
     .catch((err) => {
